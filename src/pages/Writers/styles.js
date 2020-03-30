ecd558b18719b1a0179a1832b1cb39f06282b8b4
ribeirogab <gblosr@gmail.com>
2020-03-30 15:43:43 -0300

    feat: box responsive in writer page

--- src/pages/Writers/styles.js
+++ src/pages/Writers/styles.js
@@ -36,6 +36,10 @@ export const BoxWriter = styled.div`
   width: 49%;
   margin-bottom: 2%;
 
+  @media screen and (max-width: 767px) {
+    width: 100%;
+  }
+
   .box {
     display: flex;
     flex-direction: row;
