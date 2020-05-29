a6cf4c17e8faeb6ef0635bd002b6eeb2bc1f7eb2
ribeirogab <gblosr@gmail.com>
2020-05-29 15:25:06 -0300

    feat: loader in login page

---
+++ src/components/Loader/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const Loader: React.FC = () => {
+  return (
+    <Container>
+      <div />
+    </Container>
+  );
+};
+
+export default Loader;
